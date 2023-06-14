2a2f36d0ec894add848f1f76e4fefc98c6c96617
ramirezdev6 <Santiago.Ramirez@fundacion-jala.org>
2023-06-13 21:17:15 -0500

    Tarea clase 2: Implementatcion de nueva nave enemiga Enemy_2 con su respectivo movimiento

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,4 +1,5 @@
 from game.components.enemies.enemy import Enemy
+from game.components.enemies.enemy_2 import Enemy_2
 
 
 class EnemyManager:
@@ -8,6 +9,7 @@ class EnemyManager:
     def update(self):
         if not self.enemies: # [] {} 0 "" -> false | [1] {1: 1} 1 -2 "a" -> true
             self.enemies.append(Enemy())
+            self.enemies.append(Enemy_2())
 
         for enemy in self.enemies:
             enemy.update(self.enemies)
